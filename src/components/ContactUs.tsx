4c44235e6f9a0b4864b2403711fd300417164455
lee14k <kmkovach@mtu.edu>
2023-08-14 14:07:10 -0400

    contact troubleshooting

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -80,9 +80,8 @@ export default function ContactUs() {
             <div class="mt-2.5">
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
+                name="firstName"
+                id="firstName"
                 bind={firstName}
                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
               />
