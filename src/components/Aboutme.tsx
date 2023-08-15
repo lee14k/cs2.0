924f6a0e2af5b0fa28f54c47612eed00e89e1aa9
lee14k <kmkovach@mtu.edu>
2023-08-15 11:29:41 -0400

    getting rid of cta

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -178,70 +178,7 @@ export default function Aboutme() {
         {/* Values section */}
        
         {/* Team section */}
-        <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our team</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-300">
-              Excepturi repudiandae alias ut. Totam aut facilis. Praesentium in neque vel omnis. Eos error odio. Qui
-              fugit voluptatibus eum culpa.
-            </p>
-          </div>
-          <ul
-            role="list"
-            class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
-          >
-            {team.map((person) => (
-              <li key={person.name}>
-                <img class="aspect-[14/13] w-full rounded-2xl object-cover" src={person.imageUrl} alt="" />
-                <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{person.name}</h3>
-                <p class="text-base leading-7 text-gray-300">{person.role}</p>
-                <p class="text-sm leading-6 text-gray-500">{person.location}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* CTA section */}
-        <div class="relative isolate -z-10 mt-32 sm:mt-40 pb-0.5">
-          <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-              <img
-                class="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-                src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt=""
-              />
-              <div class="w-full flex-auto">
-                <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Join our team</h2>
-                <p class="mt-6 text-lg leading-8 text-gray-300 ">
-                  Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis
-                  in accusamus quisquam.
-                </p>
-                <ul
-                  role="list"
-                  class="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2"
-                >
-                  {benefits.map((benefit) => (
-                    <li key={benefit} class="flex gap-x-3">
-                      
-                      {benefit}
-                    </li>
-                  ))}
-                </ul>
-                <div class="mt-10 flex">
-                  <a href="#" class="text-sm font-semibold leading-6 text-indigo-400">
-                    See our job postings <span aria-hidden="true">&rarr;</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            class="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl pb-0.5"
-            aria-hidden="true"
-          >
-         
-          </div>
-        </div>
+        
       </main>
 
       {/* Footer */}
