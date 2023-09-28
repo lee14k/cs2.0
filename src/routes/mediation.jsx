4f2cd2854989a4e5f1fdee52315c3d79b1edd6e4
lee14k <kmkovach@mtu.edu>
2023-09-28 12:35:13 -0400

    mediation copy

--- src/routes/mediation.jsx
+++ src/routes/mediation.jsx
@@ -1,39 +1,56 @@
-import Pricing from "../components/Pricing";
+import { createSignal, createEffect } from 'solid-js';
+import axios from 'axios';
+import Pricing from '../components/Pricing';
+
+async function fetchACFData() {
+  const apiUrl = `https://kaileehamre.net/wp-json/wp/v2/mediation/64`;
+
+  try {
+    const response = await axios.get(apiUrl);
+
+    if (typeof response.data === 'object' && response.data.acf) {
+      return response.data.acf;
+    } else {
+      console.error('Error: ACF data not found in response.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching ACF data:', error);
+    return null;
+  }
+}
+
+const paragraphs = [
+  { fieldName: 'textfieldone', description: '' },
+  { fieldName: 'textfieldtwo', description: '' },
+  { fieldName: 'textfieldthree', description: '' },
+  { fieldName: 'textfieldfour', description: '' },
+  { fieldName: 'textfieldfive', description: '' },
+];
+
 export default function Mediation() {
+  const [acfData, setACFData] = createSignal({});
+  const [paragraphsData, setParagraphsData] = createSignal(paragraphs);
+ createEffect(async () => {
+    const fetchedACFData = await fetchACFData();
+    if (fetchedACFData !== null) {
+      setACFData(fetchedACFData);
+      console.log(fetchedACFData)
+
+      const updatedParagraphsData = paragraphsData().map((paragraph) => ({
+        ...paragraph,
+        description: fetchedACFData[paragraph.fieldName] || '',
+      }));
+console.log(updatedParagraphsData)
+      setParagraphsData(updatedParagraphsData);
+    }
+  });
+
+
   return (
     <div>
       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-16 lg:overflow-visible lg:px-0">
-        <div className="absolute inset-0 -z-10 overflow-hidden">
-          <svg
-            className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-                width={200}
-                height={200}
-                x="50%"
-                y={-1}
-                patternUnits="userSpaceOnUse"
-              >
-                <path d="M100 200V.5M.5 .5H200" fill="none" />
-              </pattern>
-            </defs>
-            <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-              <path
-                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                strokeWidth={0}
-              />
-            </svg>
-            <rect
-              width="100%"
-              height="100%"
-              strokeWidth={0}
-              fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
-            />
-          </svg>
-        </div>
+     
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
@@ -46,12 +63,11 @@ export default function Mediation() {
                 <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                   Mediation
                 </h1>
-                <p className="mt-6 text-xl leading-8 text-gray-700">
-                  Regardless of the specific issue, we provide comprehensive
-                  advocacy that allows you to focus on your business. Our firm
-                  assists small and large companies in all types of conflict
-                  resolution{" "}
-                </p>
+           <p className="mt-6 text-xl leading-8 text-gray-700">
+                      {paragraphsData()[0].description || "Default content if not fetched"}
+
+            
+</p>
               </div>
             </div>
           </div>
@@ -66,9 +82,8 @@ export default function Mediation() {
             <div className="lg:pr-4">
               <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
                 <p>
-                  At our core, Conflict Solutions aims to go back to basics and
-                  offer parties who desire a non-litigated solution to their
-                  problems an extremely hard day's work for a fair price.
+                               {paragraphsData()[1].description || "Default content if not fetched"} 
+
                 </p>
                 <ul role="list" className="mt-8 space-y-8 text-gray-600">
                   <li className="flex gap-x-3">
@@ -76,8 +91,7 @@ export default function Mediation() {
                       <strong className="font-semibold text-gray-900">
                         Cost-Effective{" "}
                       </strong>{" "}
-                      Mediation provides a cost-effective alternative to
-                      expensive litigation.
+                    {paragraphsData()[2].description || "Default content if not fetched"} 
                     </span>
                   </li>
 
@@ -86,18 +100,12 @@ export default function Mediation() {
                       <strong className="font-semibold text-gray-900">
                         Confidential & Efficient :{" "}
                       </strong>{" "}
-                      We ensure that sensitive company information remains
-                      private, and we provide a swift resolution compared to
-                      extended legal battles.
+                     {paragraphsData()[3].description || "Default content if not fetched"} 
                     </span>
                   </li>
                 </ul>
                 <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien
-                  duis odio id et. Id blandit molestie auctor fermentum
-                  dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
-                  varius vulputate et ultrices hac adipiscing egestas. Iaculis
-                  convallis ac tempor et ut. Ac lorem vel integer orci.
+            {paragraphsData()[4].description || "Default content if not fetched"} 
                 </p>
               </div>
             </div>
